087cb8526d328bf67d5d57289a4d10aafc95f736
Lucas Fillype <lucasfillype00@gmail.com>
2025-11-01 00:07:12 -0300

    add car page

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const fullName = document.getElementById("fullname")
+const buttonSubmit = document.getElementById("button-submit")
+
+function submitForm(){
+    const fullName  = document.getElementByIdt("fullname").value;
+    const buttonSubmit = document.getElementById("button-submit")
+
+    
+}
